39e11931b672015e3036d4ebafc4713102afb2ce
RomenJ <justiciaterapeutica@gmail.com>
2023-07-23 17:03:08 +0200

    adding archive

---
+++ prueba1.py
@@ -0,0 +1,9 @@
+print("Hola Mundo EN Pyhton")
+print("Querido mundo, me vas a decir que ya directamente se crea el eejcutable?")
+print("REally Mundo?")
+print("TABLA DE MULTIPLICAR DE UN NÚMERO")
+num=int(input("Insert a number: "))
+for  i in range(0,11):
+    result=i*num 
+    print(("%d x %d = %d")%(num,i,result))
+
